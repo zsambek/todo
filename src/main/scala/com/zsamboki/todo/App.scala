686056bdb015ee3926baf3ffc3668a72cedbe0a2
Andras Zsamboki <zsambeek.andras@gmail.com>
2019-12-09 22:27:41 +0100

    Change from F to IO in IOApp, because it is unnecessary in this point

--- src/main/scala/com/zsamboki/todo/App.scala
+++ src/main/scala/com/zsamboki/todo/App.scala
@@ -1,30 +1,30 @@
 package com.zsamboki.todo
 
-import cats.effect.{Blocker, ConcurrentEffect, ContextShift, ExitCode, IO, IOApp, Resource, Timer}
+import cats.effect.{Blocker, ExitCode, IO, IOApp, Resource}
+import cats.syntax.functor._
 import com.zsamboki.todo.config.{DatabaseConfig, TodoConfig}
 import com.zsamboki.todo.domain.todos.TodoService
 import com.zsamboki.todo.infrastructure.endpoint.TodoEndpoints
+import com.zsamboki.todo.infrastructure.repository.TodoRepositoryInterpreter
 import doobie.util.ExecutionContexts
 import io.circe.config.parser
-import org.http4s.server.{Router, Server}
-import org.http4s.server.blaze.BlazeServerBuilder
-import cats.syntax.functor._
-import com.zsamboki.todo.infrastructure.repository.TodoRepositoryInterpreter
 import org.http4s.implicits._
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.{Router, Server}
 
 object App extends IOApp {
 
-  def createServer[F[_]: ContextShift: ConcurrentEffect: Timer]: Resource[F, Server[F]] =
+  def createServer: Resource[IO, Server[IO]] =
     for {
-      config <- Resource.liftF(parser.decodePathF[F, TodoConfig]("todo"))
-      dbEc <- ExecutionContexts.fixedThreadPool[F](config.databaseConfig.poolSize)
-      blockingEc <- ExecutionContexts.cachedThreadPool[F]
-      xa <- DatabaseConfig.transactor(config.databaseConfig, dbEc, Blocker.liftExecutionContext(blockingEc))
+      config <- Resource.liftF(parser.decodePathF[IO, TodoConfig]("todo"))
+      dbEc <- ExecutionContexts.fixedThreadPool[IO](config.databaseConfig.poolSize)
+      blockingEc <- ExecutionContexts.cachedThreadPool[IO]
+      xa <- DatabaseConfig.transactor[IO](config.databaseConfig, dbEc, Blocker.liftExecutionContext(blockingEc))
       todoRepository = TodoRepositoryInterpreter(xa)
       todoService = TodoService(todoRepository)
       httpApp = Router("/" -> TodoEndpoints.endpoints(todoService)).orNotFound
-      _ <- Resource.liftF(DatabaseConfig.init(config.databaseConfig))
-      server <- BlazeServerBuilder[F]
+      _ <- Resource.liftF(DatabaseConfig.init[IO](config.databaseConfig))
+      server <- BlazeServerBuilder[IO]
         .bindHttp(config.serverConfig.port, config.serverConfig.host)
         .withHttpApp(httpApp)
         .resource
